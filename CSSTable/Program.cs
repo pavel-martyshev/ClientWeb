529849a94beedeb3fae8491e461cea66d37d7441
Pavel <pasha.martyshev@gmail.com>
2025-09-28 16:50:45 +0300

    Add UseStaticFiles()

--- CSSTable/Program.cs
+++ CSSTable/Program.cs
@@ -7,7 +7,8 @@ namespace CSSTable
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.UseStaticFiles();
+            app.UseDefaultFiles();
 
             app.Run();
         }
