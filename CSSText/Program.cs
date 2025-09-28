a6c5cce30f15c86e439701075db4b303a6f63225
Pavel <pasha.martyshev@gmail.com>
2025-09-28 16:50:11 +0300

    Add UseStaicFiles()

--- CSSText/Program.cs
+++ CSSText/Program.cs
@@ -7,7 +7,8 @@ namespace CSSText
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.UseStaticFiles();
+            app.UseDefaultFiles();
 
             app.Run();
         }
