94af1a5381c39cac297f684dc1fcb876a67f5c66
Pavel <pasha.martyshev@gmail.com>
2025-09-28 18:34:35 +0300

    Complete CSS Form task

---
+++ CSSForm/Program.cs
@@ -0,0 +1,16 @@
+namespace CSSForm
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.UseStaticFiles();
+            app.UseDefaultFiles();
+
+            app.Run();
+        }
+    }
+}
