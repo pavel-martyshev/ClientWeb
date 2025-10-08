fbebcfd908cdb698bde20d1309cb7a42cacd2e51
Pavel <pasha.martyshev@gmail.com>
2025-10-08 21:02:22 +0300

    Fix and refactor WorkingWithArrays task

--- WorkingWithArrays/wwwroot/scripts/scripts.js
+++ WorkingWithArrays/wwwroot/scripts/scripts.js
@@ -1,35 +1,45 @@
 ﻿"use strict";
 
-(function() {
-    const array = Array.from({ length: 10 }, (_, i) => i + 1);
-    array.reverse();
+(function () {
+    function getNumbersArray(length) {
+        return Array.from({ length: length }, (_, i) => i + 1);
+    }
 
-    const firstFiveElements = array.slice(0, 5);
-    const lastFiveElements = array.slice(5);
+    function getFirstFiveElements(array) {
+        return array.slice(0, 5);
+    }
+
+    function getLastFiveElements(array) {
+        return array.slice(-5);
+    }
+
+    function getAllEvenNumbersSum(array) {
+        return array.reduce((sum, number) => {
+            if (number % 2 === 0) {
+                return sum + number;
+            }
+
+            return sum;
+        }, 0);
+    }
+
+    function getEvenNumbersSquares(array) {
+        return Array.from(
+            array.filter((number) => number % 2 === 0),
+            (number) => number * number
+        );
+    }
+
+    const numbersArray = getNumbersArray(10);
+    numbersArray.sort((a, b) => b - a);
+
+    const firstFiveElements = getFirstFiveElements(numbersArray);
+    const lastFiveElements = getLastFiveElements(numbersArray);
 
     console.log(`Первые 5 элементов: ${firstFiveElements.join(", ")}`);
     console.log(`Последние 5 элементов: ${lastFiveElements.join(", ")}`);
+    console.log(`Сумма всех четных чисел = ${getAllEvenNumbersSum(numbersArray)}`);
 
-    const sumWithInitial = array.reduce((accumulator, currentValue) => {
-        if (currentValue % 2 === 0) {
-            return accumulator + currentValue;
-        }
-
-        return accumulator;
-    }, 0);
-
-    console.log(`Сумма всех четных чисел - ${sumWithInitial}`);
-})();
-
-(function() {
-    const array = Array.from({ length: 100 }, (_, i) => i + 1);
-    const arrayEvenNumbersSquares = [];
-
-    array.reduce((_, currentValue) => {
-        if (currentValue % 2 === 0) {
-            arrayEvenNumbersSquares.push(Math.sqrt(currentValue));
-        }
-    }, 0);
-
-    console.log(`Квадраты четных чисел:\n${arrayEvenNumbersSquares.join("\n")}`);
+    const oneHundredNumbersArray = getNumbersArray(100);
+    console.log(`Квадраты четных чисел:\n${getEvenNumbersSquares(oneHundredNumbersArray).join("\n")}`);
 })();
