f002f57f178d7c60dcfcaf20a46c45cc3376df6f
Pavel <pasha.martyshev@gmail.com>
2025-10-10 19:06:51 +0300

    WorkingWithArrays/refactor

--- WorkingWithArrays/wwwroot/scripts/scripts.js
+++ WorkingWithArrays/wwwroot/scripts/scripts.js
@@ -2,18 +2,18 @@
 
 (function () {
     function getNumbersArray(length) {
-        return Array.from({ length: length }, (_, i) => i + 1);
+        return Array.from({ length }, (_, i) => i + 1);
     }
 
-    function getFirstFiveElements(array) {
-        return array.slice(0, 5);
+    function getFirstFewElements(array, elementsNumber) {
+        return array.slice(0, elementsNumber);
     }
 
-    function getLastFiveElements(array) {
-        return array.slice(-5);
+    function getLastFewElements(array, elementsNumber) {
+        return array.slice(-elementsNumber);
     }
 
-    function getAllEvenNumbersSum(array) {
+    function getEvenNumbersSum(array) {
         return array.reduce((sum, number) => {
             if (number % 2 === 0) {
                 return sum + number;
@@ -24,21 +24,20 @@
     }
 
     function getEvenNumbersSquares(array) {
-        return Array.from(
-            array.filter(number => number % 2 === 0),
-            number => number * number
-        );
+        return array
+            .filter(number => number % 2 === 0)
+            .map(number => number * number);
     }
 
     const numbersArray = getNumbersArray(10);
     numbersArray.sort((a, b) => b - a);
 
-    const firstFiveElements = getFirstFiveElements(numbersArray);
-    const lastFiveElements = getLastFiveElements(numbersArray);
+    const firstFiveElements = getFirstFewElements(numbersArray, 5);
+    const lastFiveElements = getLastFewElements(numbersArray, 5);
 
     console.log(`Первые 5 элементов: ${firstFiveElements.join(", ")}`);
     console.log(`Последние 5 элементов: ${lastFiveElements.join(", ")}`);
-    console.log(`Сумма всех четных чисел = ${getAllEvenNumbersSum(numbersArray)}`);
+    console.log(`Сумма всех четных чисел = ${getEvenNumbersSum(numbersArray)}`);
 
     const oneHundredNumbersArray = getNumbersArray(100);
     console.log(`Квадраты четных чисел:\n${getEvenNumbersSquares(oneHundredNumbersArray).join("\n")}`);
