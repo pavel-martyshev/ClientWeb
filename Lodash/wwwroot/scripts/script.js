ac57f2aad3a376aeee106378c2ba2998a9c1da46
Pavel <pasha.martyshev@gmail.com>
2025-10-12 19:38:54 +0300

    Lodash/complete task

---
+++ Lodash/wwwroot/scripts/script.js
@@ -0,0 +1,43 @@
+﻿"use strict";
+
+(function () {
+    const people = [
+        { name: "Дмитрий", age: 28 },
+        { name: "Елена", age: 24 },
+        { name: "Сергей", age: 35 },
+        { name: "Ольга", age: 31 },
+        { name: "Ольга", age: 42 },
+        { name: "Анна", age: 26 },
+        { name: "Елена", age: 33 },
+        { name: "Наталья", age: 29 },
+        { name: "Игорь", age: 39 },
+        { name: "Дмитрий", age: 27 }
+    ];
+
+    console.log(`Средний возраст всех людей: ${_.meanBy(people, p => p.age)}`);
+
+    console.log(
+        _.chain(people)
+            .filter(p => {
+                if (p.age >= 20 && p.age <= 30) {
+                    return p;
+                }
+            })
+            .orderBy(["age"])
+            .value()
+    );
+
+    console.log(
+        _.chain(people)
+            .uniqBy(p => p.name)
+            .filter(p => {
+                if (p.age >= 20 && p.age <= 30) {
+                    return p;
+                }
+            })
+            .orderBy(["age"], ["desc"])
+            .value()
+    );
+
+    console.log(_.countBy(people, "name"));
+})();
